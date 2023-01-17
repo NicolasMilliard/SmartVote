8eaa06aa40ff4f9b737d0fbd992c9da6884a23de
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-01-17 14:38:52 +0100

    Add new dependencies for testing, coverage and env

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,9 +1,18 @@
 require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@openzeppelin/hardhat-upgrades");
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
     version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
   },
   paths: {
     sources: "./contracts",
