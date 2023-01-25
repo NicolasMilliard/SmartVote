70550b7e5143915e755fd12a48aa4248c6310514
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-01-25 11:32:41 +0100

    Upd RainbowKit add localhost and SmartVoteProvider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,17 +1,23 @@
 import React, { FC } from "react";
 import type { AppProps } from "next/app";
 
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { polygonMumbai } from "wagmi/chains";
+import { localhost, polygonMumbai } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
 import Layout from "../components/Layout/Layout";
+import { SmartVoteProvider } from "../context";
 
 import "@rainbow-me/rainbowkit/styles.css";
+import "../styles/global.css";
 
 const { chains, provider } = configureChains(
-  [polygonMumbai],
+  [localhost, polygonMumbai],
   [publicProvider()]
 );
 
@@ -21,7 +27,7 @@ const { connectors } = getDefaultWallets({
 });
 
 const wagmiClient = createClient({
-  autoConnect: false,
+  autoConnect: true,
   connectors,
   provider,
 });
@@ -29,10 +35,20 @@ const wagmiClient = createClient({
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} modalSize="compact">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+      <RainbowKitProvider
+        chains={chains}
+        modalSize="compact"
+        theme={darkTheme({
+          accentColor:
+            "linear-gradient(90deg, #FCD100 0%, #FD9A0B 47.4%, #FD3920 100%);",
+          accentColorForeground: "white",
+        })}
+      >
+        <SmartVoteProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SmartVoteProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   );
