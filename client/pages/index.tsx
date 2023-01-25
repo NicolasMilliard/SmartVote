ee70138249365181f806847ad388665ee8e12211
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-01-25 11:32:51 +0100

    Init commit

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import ConnectWallet from "../components/Layout/Buttons/ConnectWallet";
+import { useAccount } from "wagmi";
+
 function HomePage() {
-  return <div>Welcome to SmartVote!</div>;
+  const { address } = useAccount();
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  // Check if user is connected
+  const checkCurrentAccount = () => {
+    if (address) {
+      setCurrentAccount(address);
+    } else {
+      console.log(`address: ${address}`);
+    }
+  };
+
+  useEffect(() => {
+    checkCurrentAccount();
+    console.log(currentAccount);
+  }, [currentAccount]);
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold text-orange">SmartVote!</h1>
+      <p className="text-white mt-8 mb-10">
+        Connect your wallet to access your dashboard.
+      </p>
+      <ConnectWallet />
+    </div>
+  );
 }
 
 export default HomePage;
