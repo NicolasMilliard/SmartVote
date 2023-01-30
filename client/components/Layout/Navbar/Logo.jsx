5ecda0b0090c897959421cf3f1539c2fa9bdfc20
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-01-30 19:19:12 +0100

    Logo is now responsive

--- client/components/Layout/Navbar/Logo.jsx
+++ client/components/Layout/Navbar/Logo.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 const Logo = () => {
   return (
     <div className="flex items-center">
-      <div className="w-[4rem] h-[4rem] bg-black rounded flex items-center justify-center">
-        <div className="text-4xl text-white">SV</div>
+      <div className="w-[3rem] sm:w-[4rem] h-[3rem] sm:h-[4rem] bg-black rounded flex items-center justify-center">
+        <div className="text-2xl sm:text-4xl text-white">SV</div>
       </div>
-      <div className="ml-6 text-5xl font-bold">SmartVote</div>
+      <div className="ml-6 text-3xl sm:text-5xl font-bold">SmartVote</div>
     </div>
   );
 };
