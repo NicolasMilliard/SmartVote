42300cd5e69ac799c6738579b4177ff90c93b97f
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-01-31 19:17:08 +0100

    Add hover style

--- client/components/Buttons/Button.jsx
+++ client/components/Buttons/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ text, customFunction }) => {
     <button
       type="button"
       onClick={customFunction}
-      className="bg-black text-white font-bold py-4 px-6 rounded"
+      className="bg-black text-white font-bold py-4 px-6 rounded hover:bg-white hover:text-black outline hover:outline-black hover:outline-2"
     >
       {text}
     </button>
