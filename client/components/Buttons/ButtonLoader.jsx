065400aa4e5c1e87c0fce94027bffd04a15afb7d
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-01-31 19:17:28 +0100

    Update CSS rules and spinner

--- client/components/Buttons/ButtonLoader.jsx
+++ client/components/Buttons/ButtonLoader.jsx
@@ -1,8 +1,8 @@
 const ButtonLoader = () => {
   return (
-    <div className="font-bold py-2 px-4 rounded-xl cursor-not-allowed">
+    <div className="bg-gray-900 text-white font-bold py-4 px-6 rounded cursor-not-allowed">
       <span className="flex items-center">
-        <span className="spin-loader"></span>Loading...
+        <span className="btn-spin-loader"></span>Loading...
       </span>
     </div>
   );
