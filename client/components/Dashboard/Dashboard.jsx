3ad86477eefc12aee4b69f5289a35921671b6d81
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-02-02 16:13:00 +0100

    Reorganize dashboard

--- client/components/Dashboard/Dashboard.jsx
+++ client/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,79 @@
+import { useState } from "react";
+
 import Menu from "../Layout/Menu/Menu";
-import Navbar from "../Layout/Navbar/Navbar";
 import VotingSessionCardsList from "./VotingSessionCardsList";
+import Button from "../Buttons/Button";
+import ButtonLoader from "../Buttons/ButtonLoader";
+
+import { useSmartVote } from "../../context";
 
 const Dashboard = () => {
+  const {
+    state: { votingFactoryContract },
+  } = useSmartVote();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [currentRole, setCurrentRole] = useState(0);
+
+  const rolesArray = [
+    { id: 0, title: "Administrator" },
+    { id: 1, title: "Voter" },
+    { id: 2, title: "Non Voter" },
+  ];
+
+  const handleRole = (id) => {
+    setCurrentRole(id);
+  };
+
+  const createInstance = async () => {
+    try {
+      if (!votingFactoryContract) return;
+
+      setIsLoading(true);
+      setIsUpdated(false);
+
+      const tx = await votingFactoryContract.b_A6Q();
+      await tx.wait();
+
+      setIsLoading(false);
+      setIsUpdated(true);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <Menu />
       <section className="my-20 mx-8 md:mx-20 xl:mx-40">
-        <Navbar />
-        <VotingSessionCardsList />
+        <nav className="flex justify-between">
+          <div className="flex gap-20">
+            {rolesArray.map((role) => (
+              <button
+                key={role.id}
+                onClick={() => handleRole(role.id)}
+                className={
+                  role.id === currentRole
+                    ? "font-bold text-black text-2xl role-active"
+                    : "font-bold text-gray-900 hover:text-black text-2xl"
+                }
+              >
+                {role.title}
+              </button>
+            ))}
+          </div>
+          {isLoading ? (
+            <ButtonLoader />
+          ) : (
+            <Button
+              text="Create a new Voting Session"
+              customFunction={createInstance}
+            />
+          )}
+        </nav>
+        <VotingSessionCardsList role={currentRole} reloadList={isUpdated} />
       </section>
     </>
   );
