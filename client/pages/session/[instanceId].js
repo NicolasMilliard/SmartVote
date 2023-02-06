1a77135d5b0d61a16f65a5084e6601d7a0de7048
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-02-06 19:41:00 +0100

    Rename instance for testing dashboard

--- client/pages/session/[instanceId].js
+++ client/pages/session/[instanceId].js
@@ -1,10 +1,35 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { useSmartVote } from "../../context";
+import Button from "../../components/Buttons/Button";
+import ButtonLoader from "../../components/Buttons/ButtonLoader";
 
 const Instance = () => {
   const router = useRouter();
   const { instanceId } = router.query;
+  const [instanceName, setInstanceName] = useState("Bonjour");
+  const {
+    state: { getVotingHandler },
+  } = useSmartVote();
 
-  return <p>Instance: {instanceId}</p>;
+  const renameInstance = async () => {
+    try {
+      if (!getVotingHandler) return;
+
+      const contract = await getVotingHandler(instanceId);
+
+      const tx = await contract.renameInstance(instanceName);
+      await tx.wait();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Button text="Rename instance" customFunction={renameInstance} />
+    </>
+  );
 };
 
 export default Instance;
