89a6853b6a9fdf32419429d33790466bdc545c16
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-02-08 19:45:48 +0100

    Adding theme to Button

--- client/components/Buttons/Button.jsx
+++ client/components/Buttons/Button.jsx
@@ -1,13 +1,30 @@
-const Button = ({ text, customFunction }) => {
-  return (
-    <button
-      type="button"
-      onClick={customFunction}
-      className="bg-black text-white font-bold py-4 px-6 rounded hover:bg-white hover:text-black outline hover:outline-black hover:outline-2"
-    >
-      {text}
-    </button>
-  );
+const Button = ({ text, customFunction, theme }) => {
+  const themeButton = (theme) => {
+    switch (theme) {
+      case "red":
+        return (
+          <button
+            type="button"
+            onClick={customFunction}
+            className="bg-red text-white font-bold py-4 px-6 rounded hover:bg-white hover:text-red outline hover:outline-red hover:outline-2"
+          >
+            {text}
+          </button>
+        );
+      default:
+        return (
+          <button
+            type="button"
+            onClick={customFunction}
+            className="bg-black text-white font-bold py-4 px-6 rounded hover:bg-white hover:text-black outline hover:outline-black hover:outline-2"
+          >
+            {text}
+          </button>
+        );
+    }
+  };
+
+  return <>{themeButton(theme)}</>;
 };
 
 export default Button;
