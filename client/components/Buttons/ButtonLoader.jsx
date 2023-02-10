017fbecd2574384e87b4a9eebbc0fa935e04d28e
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-02-10 16:29:31 +0100

    Add theme to ButtonLoader

--- client/components/Buttons/ButtonLoader.jsx
+++ client/components/Buttons/ButtonLoader.jsx
@@ -1,11 +1,34 @@
-const ButtonLoader = () => {
-  return (
-    <div className="bg-gray-900 text-white font-bold py-4 px-6 rounded cursor-not-allowed">
-      <span className="flex items-center">
-        <span className="btn-spin-loader"></span>Loading...
-      </span>
-    </div>
-  );
+const ButtonLoader = ({ theme }) => {
+  const themeButton = (theme) => {
+    switch (theme) {
+      case "red":
+        return (
+          <div className="bg-red text-white font-bold py-4 px-6 rounded cursor-not-allowed">
+            <span className="flex items-center">
+              <span
+                className="btn-spin-loader"
+                style={{ borderTopColor: "#e84949" }}
+              ></span>
+              Loading...
+            </span>
+          </div>
+        );
+      default:
+        return (
+          <div className="bg-gray-900 text-white font-bold py-4 px-6 rounded cursor-not-allowed">
+            <span className="flex items-center">
+              <span
+                className="btn-spin-loader"
+                style={{ borderTopColor: "#5c5b63" }}
+              ></span>
+              Loading...
+            </span>
+          </div>
+        );
+    }
+  };
+
+  return <>{themeButton(theme)}</>;
 };
 
 export default ButtonLoader;
