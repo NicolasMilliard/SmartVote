c85717ebb40b58732230a06ee7b3e327ded90f55
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-02-20 17:18:53 +0100

    Logo has now a Link

--- client/components/Layout/Logo.jsx
+++ client/components/Layout/Logo.jsx
@@ -1,11 +1,15 @@
+import Link from "next/link";
+
 const Logo = () => {
   return (
-    <div className="flex items-center">
-      <div className="w-[3rem] sm:w-[4rem] h-[3rem] sm:h-[4rem] bg-black rounded flex items-center justify-center">
-        <div className="text-2xl sm:text-4xl text-white">SV</div>
+    <Link href="/">
+      <div className="flex items-center">
+        <div className="w-[3rem] sm:w-[4rem] h-[3rem] sm:h-[4rem] bg-black rounded flex items-center justify-center">
+          <div className="text-2xl sm:text-4xl text-white">SV</div>
+        </div>
+        <div className="ml-6 text-3xl sm:text-5xl font-bold">SmartVote</div>
       </div>
-      <div className="ml-6 text-3xl sm:text-5xl font-bold">SmartVote</div>
-    </div>
+    </Link>
   );
 };
 
