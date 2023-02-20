e3c53899df4411042625a186d4b15a40194ea632
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-02-20 17:19:18 +0100

    Add status and rename functionnalities

--- client/pages/session/[instanceId].js
+++ client/pages/session/[instanceId].js
@@ -1,51 +1,107 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useSmartVote } from "../../context";
-import Button from "../../components/Buttons/Button";
-import ButtonLoader from "../../components/Buttons/ButtonLoader";
 import { useAccount } from "wagmi";
+
+import { getInstanceStatus } from "../../utils/instance/getInstanceStatus";
+import { getWorkflowStatus } from "../../utils/instance/getWorkflowStatus";
+import { getInstanceName } from "../../utils/instance/getInstanceName";
+
+import Menu from "../../components/Layout/Menu/Menu";
+import Role from "../../components/Instance/Role";
+import VotingSessionDeletePopUp from "../../components/Dashboard/VotingSessionCards/VotingSessionDeletePopUp";
+import ButtonDelete from "../../components/Buttons/ButtonDelete";
+import InstanceDeleted from "../../components/Instance/InstanceDeleted";
+import WorkflowStatus from "../../components/Instance/WorkflowStatus";
+import InstanceName from "../../components/Instance/InstanceName";
+import AddVoter from "../../components/Instance/AddVoter";
 
 const Instance = () => {
   const router = useRouter();
   const { instanceId } = router.query;
-  const [instanceName, setInstanceName] = useState("Bonjour");
+  const [instanceStatus, setInstanceStatus] = useState(false);
+  const [workflowStatus, setWorkflowStatus] = useState(0);
+  const [instanceName, setInstanceName] = useState("");
+  const [deleteInstancePopUp, setDeleteInstancePopUp] = useState(false);
   const {
     state: { getVotingHandler },
   } = useSmartVote();
   const { address } = useAccount();
 
-  // Rename instance
-  const renameInstance = async () => {
-    try {
-      if (!getVotingHandler) return;
-
-      const contract = await getVotingHandler(instanceId);
-
-      const tx = await contract.renameInstance(instanceName);
-      await tx.wait();
-    } catch (error) {
-      console.log(error);
-    }
+  // Check if instance was delete
+  const handleInstanceStatus = async () => {
+    setInstanceStatus(await getInstanceStatus(getVotingHandler, instanceId));
   };
 
-  // Add a voter
-  const addVoter = async () => {
-    try {
-      if (!getVotingHandler) return;
+  useEffect(() => {
+    handleInstanceStatus();
+  }, [getVotingHandler, instanceId, instanceStatus]);
 
-      const contract = await getVotingHandler(instanceId);
+  // Check the current workflow of the status
+  const handleWorkflowStatus = async () => {
+    setWorkflowStatus(await getWorkflowStatus(getVotingHandler, instanceId));
+  };
 
-      const tx = await contract.authorize(address);
-      await tx.wait();
-    } catch (error) {
-      console.log(error);
-    }
+  useEffect(() => {
+    handleWorkflowStatus();
+  }, [getVotingHandler, instanceId, workflowStatus]);
+
+  // Check the current name of the instance
+  const handleInstanceName = async () => {
+    setInstanceName(await getInstanceName(getVotingHandler, instanceId));
+  };
+
+  useEffect(() => {
+    handleInstanceName();
+  }, [getVotingHandler, instanceId, instanceName]);
+
+  // Toggle pop-up
+  const togglePopUp = () => {
+    setDeleteInstancePopUp(!deleteInstancePopUp);
   };
 
   return (
     <>
-      <Button text="Rename instance" customFunction={renameInstance} />
-      <Button text="Add voter" customFunction={addVoter} />
+      <Menu isInstance={true} />
+      <section className="my-8 sm:my-20 mx-8 md:mx-20 xl:mx-40">
+        {instanceStatus ? (
+          <InstanceDeleted contractAddress={instanceId} />
+        ) : (
+          <>
+            {deleteInstancePopUp && (
+              <VotingSessionDeletePopUp
+                contractAddress={instanceId}
+                instanceName={instanceName}
+                togglePopUp={togglePopUp}
+              />
+            )}
+
+            <div className="flex justify-between mb-10">
+              <Role
+                getVotingHandler={getVotingHandler}
+                userAddress={address}
+                contractAddress={instanceId}
+              />
+              <ButtonDelete
+                text="Delete session"
+                customFunction={togglePopUp}
+              />
+            </div>
+            <section className="sm:flex gap-16">
+              <div>
+                <WorkflowStatus active={workflowStatus} />
+              </div>
+              <div className="flex-col">
+                <InstanceName
+                  instanceName={instanceName}
+                  contractAddress={instanceId}
+                />
+                <AddVoter />
+              </div>
+            </section>
+          </>
+        )}
+      </section>
     </>
   );
 };
