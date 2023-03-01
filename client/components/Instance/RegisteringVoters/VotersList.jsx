99be1d5dcd6b2c6b79ac1f7dcf767c9ed60e4123
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-03-01 18:08:08 +0100

    Take userAddress as a prop

--- client/components/Instance/RegisteringVoters/VotersList.jsx
+++ client/components/Instance/RegisteringVoters/VotersList.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 import { getVoters } from "../../../utils/instance/RegisteringVoters/getVoters";
 
-const VotersList = ({ getVotingHandler, contractAddress }) => {
+const VotersList = ({ getVotingHandler, contractAddress, userAddress }) => {
   const [voters, setVoters] = useState([]);
 
   const getVotersList = async () => {
@@ -17,7 +17,10 @@ const VotersList = ({ getVotingHandler, contractAddress }) => {
       <h3 className="font-semibold text-xl mb-6">Voters addresses:</h3>
       <ul className="flex flex-col gap-4 text-gray-900">
         {voters.map((voter) => (
-          <li key={voter.address}>
+          <li
+            key={voter.address}
+            className={`${userAddress == voter.address && "font-semibold"}`}
+          >
             {voter.id}&nbsp;: {voter.address}
           </li>
         ))}
