4d00e6c48d4a2a9a123e31ac98aca00b4d7f4422
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-03-01 18:09:37 +0100

    Correct the call to the SC

--- client/utils/instance/getRole.js
+++ client/utils/instance/getRole.js
@@ -1,7 +1,7 @@
 export const getRole = async (
   getVotingHandler,
-  userAddress,
-  contractAddress
+  contractAddress,
+  userAddress
 ) => {
   try {
     if (!getVotingHandler) return;
@@ -9,17 +9,15 @@ export const getRole = async (
     const contract = await getVotingHandler(contractAddress);
     const tx = await contract.owner();
 
-    if (tx === userAddress) {
+    // Check if the user is the admin
+    if (tx == userAddress) {
       return 0;
     } else {
-      const votersEventFilter =
-        votingHandlerContract.filters.VoterRegistered(userAddress);
-      const votersEvents = await votingHandlerContract.queryFilter(
-        votersEventFilter,
-        0
-      );
+      const votersEventFilter = contract.filters.VoterRegistered(userAddress);
+      const votersEvents = await contract.queryFilter(votersEventFilter, 0);
 
-      if (votersEvents === userAddress) {
+      // Check if the user is a voter or not
+      if (votersEvents[0].args[0] == userAddress) {
         return 1;
       } else {
         return 2;
