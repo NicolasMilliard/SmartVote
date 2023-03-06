345c61e29c27eb1bb8c1d4006d38b6d1604e1f3e
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-03-06 17:02:38 +0100

    Add Head and favicon

--- client/components/Layout/Layout.jsx
+++ client/components/Layout/Layout.jsx
@@ -1,5 +1,15 @@
+import Head from "next/head";
+
 const Layout = ({ children }) => {
-  return <main>{children}</main>;
+  return (
+    <>
+      <Head>
+        <title>Smart Vote</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
+      </Head>
+      <main>{children}</main>
+    </>
+  );
 };
 
 export default Layout;
